b943bed34359500ecfa5a244de5da91e06a0c4d5
tito <tito.rikalainen@gmail.com>
2023-04-16 11:56:58 +0300

    news and stories

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-const express = require("express"),
-app = express(),
-router = express.Router(),
-layouts = require("express-ejs-layouts"),
-mongoose = require("mongoose"),
-methodOverride = require("method-override"),
-expressSession = require("express-session"),
-cookieParser = require("cookie-parser"),
-connectFlash = require("connect-flash"),
-passport = require("passport"),
-homeController = require("./controllers/homeController");
+const express = require("express");
+const app = express();
+const homeRoutes = require('./routes/homeRoutes'); //added
+
+const layouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const expressSession = require("express-session");
+const cookieParser = require("cookie-parser");
+const connectFlash = require("connect-flash");
+const passport = require("passport");
+const homeController = require("./controllers/homeController");
+
+const router = express.Router();
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
+app.use('/', homeRoutes); //modified
 
 router.use(express.static("public"));
 router.use(layouts);
@@ -42,8 +46,6 @@ router.use(
 router.use(homeController.logRequestPaths);
 router.get("/", homeController.index);
 
-
-app.use("/", router);
 app.listen(app.get("port"), () => {
   console.log(`Server running at http://localhost:${app.get("port")}`);
 });
